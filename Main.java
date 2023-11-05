030d7bdcfb4af0a4ff3d088b5b61887b403e76da
alaeddine32 <149829885+alaeddine32@users.noreply.github.com>
2023-11-05 12:45:23 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{ 
+
+    public static void main(String[] args){
+        System.out.println("My name is Aladin Hidri");
+        System.out.println("I am 26 years old");
+        System.out.println("My hometown is Tunis, Tunisia");
+    }
+
+}
